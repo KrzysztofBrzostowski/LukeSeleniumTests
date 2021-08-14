5e831308ecb16992529f2efa8784f01488f94c52
Christopher Brzostowski <kbrzostowski@gmail.com>
2021-08-14 22:17:07 +0200

    LukeSeleniumTests

---
+++ Luke.Selenium.Tests/MapsPage.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Globalization;
+
+namespace Luke.Selenium.Pages
+{
+    public class MapsPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        private By cookiesConsentFrame   = By.CssSelector(".widget-consent-frame");
+        private By cookiesConsentButton  = By.CssSelector("#introAgreeButton > span > span");
+        private By cookiesConsentOverlay = By.CssSelector(".widget-consent-fullscreen");
+        
+        private By routeButton             = By.CssSelector("#searchbox-directions");
+        private By routeStartingPointField = By.CssSelector("#sb_ifc51 > input");
+        private By routeDestinationField   = By.CssSelector("#sb_ifc52 > input");
+        
+        private By onFootRouteModeButton = By.CssSelector("div[data-travel_mode='2'] > button > img");
+        private By byBikeRouteModeButton = By.CssSelector("div[data-travel_mode='1'] > button > img");
+        
+        private By routeSearchFirstResult         = By.CssSelector("#section-directions-trip-0");
+        private By routeSearchFirstResultDuration = By.CssSelector(".section-directions-trip-duration");
+        private By routeSearchFirstResultDistance = By.CssSelector(".section-directions-trip-distance");
+
+        public MapsPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://www.google.pl/maps/");
+        }
+
+        public void AgreeToCookies()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(cookiesConsentFrame));
+            driver.SwitchTo().Frame(driver.FindElement(cookiesConsentFrame));
+            wait.Until(ExpectedConditions.ElementIsVisible(cookiesConsentButton)).Click();
+            driver.SwitchTo().DefaultContent();
+        }
+
+        public void GetFastestRoute(string startingPoint, string destination, string travelMode = "pieszo")
+        {
+            driver.FindElement(routeButton).Click();
+            IWebElement webElement = wait.Until(ExpectedConditions.ElementIsVisible(routeStartingPointField));
+            webElement.Clear();
+            webElement.SendKeys(startingPoint);
+            driver.FindElement(routeDestinationField).SendKeys(destination);
+
+            switch (travelMode)
+            {
+                case "na rowerze":
+                    driver.FindElement(byBikeRouteModeButton).Click();
+                    break;
+                default:
+                    driver.FindElement(onFootRouteModeButton).Click();
+                    break;
+            }
+
+            wait.Until(ExpectedConditions.ElementIsVisible(routeSearchFirstResult));
+        }
+
+        public int GetFastestRouteDuration()
+        {
+            string firstResultDuration = driver.FindElement(routeSearchFirstResultDuration).Text;
+            return int.Parse(firstResultDuration.Remove(firstResultDuration.IndexOf(" ")));
+        }
+
+        public float GetFastestRouteDistance()
+        {
+            string firstResultDistance = driver.FindElement(routeSearchFirstResultDistance).Text.Replace(",", ".");
+            return float.Parse(firstResultDistance.Remove(firstResultDistance.IndexOf(" ")), CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Luke.Selenium.Tests/MapsTests.cs
@@ -0,0 +1,90 @@
+﻿using NLog;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Luke.Selenium.Pages;
+using System;
+
+namespace Luke.Selenium.Tests
+{
+    [TestFixture("Chrome")]
+    [TestFixture("Firefox")]
+    class MapsTest
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private string browser;
+
+        public MapsTest(string browser)
+        {
+            this.browser = browser;
+        }
+        
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            switch (browser)
+            {
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestCase("plac Defilad 1, Warszawa", "Chłodna 51, Warszawa", "pieszo")]
+        [TestCase("plac Defilad 1, Warszawa", "Chłodna 51, Warszawa", "na rowerze")]
+        [TestCase("Chłodna 51, Warszawa", "plac Defilad 1, Warszawa", "pieszo")]
+        [TestCase("Chłodna 51, Warszawa", "plac Defilad 1, Warszawa", "na rowerze")]
+        public void TestCase(string staringPoint, string destination, string travelMode)
+        {
+            MapsPage mapsPage = new MapsPage(driver);
+            mapsPage.Open();
+            mapsPage.AgreeToCookies();
+            mapsPage.GetFastestRoute(staringPoint, destination, travelMode);
+
+            int durationLimit;
+            
+            switch (travelMode)
+            {
+                case "na rowerze":
+                    durationLimit = 15;
+                    break;
+                default:
+                    durationLimit = 40;
+                    break;
+            }
+            
+            Assert.Greater(durationLimit, mapsPage.GetFastestRouteDuration(),
+                "Czas podróży pomiędzy punktami jest większy niż " + durationLimit + " min.");
+
+            float distanceLimit = 3;
+
+            Assert.Greater(distanceLimit, mapsPage.GetFastestRouteDistance(),
+                "Odległość pomiędzy punktami jest wieksza niż " + distanceLimit + " km.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            string testResult = TestContext.CurrentContext.Result.Outcome.Status + " | " +
+                TestContext.CurrentContext.Test.FullName + Environment.NewLine;
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                testResult += TestContext.CurrentContext.Result.Message + Environment.NewLine;
+            }
+
+            logger.Info(testResult);
+            
+            driver.Quit();
+        }
+    }
+}
